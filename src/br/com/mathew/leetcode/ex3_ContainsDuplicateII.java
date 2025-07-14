da7513d605fd395e869d08be021f621c3980478f
Mathew <matheoshanna10@gmail.com>
2025-07-13 23:22:09 -0300

    feat: ex     public boolean containsNearbyDuplicate(int[] nums, int k) {

---
+++ src/br/com/mathew/leetcode/ex3_ContainsDuplicateII.java
@@ -0,0 +1,22 @@
+package br.com.mathew.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ex3_ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer , Integer> ultimoIndice  = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            Integer anterior = ultimoIndice.get(num);
+
+            if (anterior != null && i - anterior <= k) {
+                return true;
+            }
+
+            ultimoIndice.put(num, i);
+        }
+        return false;
+    }
+}
