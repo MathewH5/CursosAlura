10f929e1418208e8fb40ceb6d10102202d186200
Mathew <matheoshanna10@gmail.com>
2025-07-31 23:19:40 -0300

    feat: ex 9 incompleto leet

---
+++ src/br/com/mathew/leetcode/ex9_removeDuplicates26.java
@@ -0,0 +1,17 @@
+package br.com.mathew.leetcode;
+
+public class ex9_removeDuplicates26 {
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
+    }
+    public static int removeDuplicates(int[] nums) {
+        int tamanho = nums.length;
+        int novoTamanho= tamanho;
+
+        for (int i=0;i<tamanho;i++){
+            if (nums[i]==nums[i+1]&& i+1!=tamanho){
+
+            }
+        }
+    }
+}
